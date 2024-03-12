cfe2a01c3aed64a0d5fe286dc15738e56ca8dfcd
MadhanKumar-1 <madhan@teams.tachyonsys.com.au>
2024-03-12 15:15:07 +0530

    Initial commit

---
+++ specific-pricing-update/src/index.js
@@ -0,0 +1,17 @@
+const ENDPOINT_DATA = JSON.parse(process.env.ENDPOINT_DATA);
+exports.handler = async event => {
+  console.log(event);
+  console.log(ENDPOINT_DATA);
+  let {
+    requestContext
+  } = event;
+  let response = {
+    id: ENDPOINT_DATA.endpointId,
+    method: requestContext.http.method,
+    baseUrl: requestContext.domainName,
+    path: event.routeKey.split(" ")[1],
+    pathParam: event.pathParameters,
+    queryParam: event.queryStringParameters
+  };
+  return response;
+};
